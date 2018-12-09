eb4737e219591ca267834319967d68e9b1dd8cfb
Ian Hundere <ianfundere@gmail.com>
2018-12-09 13:31:58 -0500

    init

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className="f1 tc">
+                <h1>hello there</h1>
+                <p> welcome home</p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Hello from './Hello';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+
+serviceWorker.unregister();

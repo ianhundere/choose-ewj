bf3a7410d8823c4516b049c48da540dca9eff05a
Ian Hundere <ianfundere@gmail.com>
2018-12-10 07:46:20 -0500

    maps cards

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
-const Card = props => {
+const Card = ({ name, email, id }) => {
     return (
         <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img
-                src={`https://robohash.org/${props.id}?200x200`}
-                alt="characters"
-            />
+            <img src={`https://robohash.org/${id}?200x200`} alt="characters" />
             <div>
-                <h2>
-                    {props.name}
-                    <p>{props.email}</p>
-                </h2>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );

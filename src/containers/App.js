ed6d2050a8dd74c7a34ba5b210587d58cc386b5c
Ian Hundere <ianfundere@gmail.com>
2018-12-11 07:50:13 -0500

    organizes files and refactors code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -25,22 +25,21 @@ class App extends Component {
         this.setState({ searchfield: e.target.value });
     };
     render() {
-        const filterRobots = this.state.robots.filter(robot => {
-            return robot.name
-                .toLowerCase()
-                .includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filterRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>;
-        } else {
-            return (
-                <div className="tc">
-                    <h1 className="f1">Mech Friends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+        return !robots.length ? (
+            <h1>Loading</h1>
+        ) : (
+            <div className="tc">
+                <h1 className="f1">Mech Friends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
                     <CardList robots={filterRobots} />
-                </div>
-            );
-        }
+                </Scroll>
+            </div>
+        );
     }
 }
 export default App;
